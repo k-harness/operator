c2f2bd4469a31e7c8c301a772f6b60ffed4b1f0b
Dmitry Harupa <d7561985@gmail.com>
2021-05-29 14:21:49 +0300

    api/group: miss role changes

--- controllers/scenario_controller.go
+++ controllers/scenario_controller.go
@@ -41,9 +41,9 @@ type ScenarioReconciler struct {
 	Scheme   *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=scenarios.karness.io,resources=scenarios,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=scenarios.karness.io,resources=scenarios/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=scenarios.karness.io,resources=scenarios/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core.karness.io,resources=scenarios,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core.karness.io,resources=scenarios/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=core.karness.io,resources=scenarios/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
