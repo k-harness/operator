927f6e90c430c4cdb16b89dbd7f239cfacb6297a
Dmitry Harupa <d7561985@gmail.com>
2021-05-29 10:56:29 +0300

    api: rename group as core not scenarios

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the scenarios v1alpha1 API group
 //+kubebuilder:object:generate=true
-//+groupName=scenarios.karness.io
+//+groupName=core.karness.io
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "scenarios.karness.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "core.karness.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
